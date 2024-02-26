0f40bb9d7b59d4b0c782f419728be30c9c7a7a66
Matheus Felipe <161388418+matheusfelipx@users.noreply.github.com>
2024-02-26 20:14:03 -0300

    Segundo Commit

--- ProjectX/src/ClassePrincipal.java
+++ ProjectX/src/ClassePrincipal.java
@@ -2,8 +2,11 @@
 public class ClassePrincipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		int x = 5;
+		int y = 7;
+		
+		System.out.println(x + y );
 	}
 
 }
